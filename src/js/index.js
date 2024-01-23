914ea355197b78f4ed59697576034239812b328c
ryanp2dev <ryantrabalho2@gmail.com>
2024-01-23 19:47:12 -0400

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const $stepOne = $('.step.one')
+const $stepTwo = $('.step.two')
+const $stepThree = $('.step.three')
+
+function init() {
+    $stepTwo.hide();
+    $stepThree.hide();
+
+}
+
+init()
